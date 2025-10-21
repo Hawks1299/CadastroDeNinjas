7ded392b73e33a81982475b2a26c98940b8c6896
Hawks1299 <mastskel@gmail.com>
2025-10-21 15:05:23 -0400

    feat: Coloca o Lombok na classe para abstracao

--- src/main/java/dev/java10x/CadastroDeNinjas/Carros/CarrosModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Carros/CarrosModel.java
@@ -2,11 +2,17 @@ package dev.java10x.CadastroDeNinjas.Carros;
 
 import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_carros")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CarrosModel {
 
     @Id
@@ -20,29 +26,5 @@ public class CarrosModel {
     // @OneToMany um carro pode ter mais de uma pessoa
     @OneToMany(mappedBy = "carros")
     private List<NinjaModel> ninjas;
-
-    public CarrosModel() {
-    }
-
-    public CarrosModel(String nome, String tipo) {
-        this.nome = nome;
-        this.tipo = tipo;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
-
+    
 }

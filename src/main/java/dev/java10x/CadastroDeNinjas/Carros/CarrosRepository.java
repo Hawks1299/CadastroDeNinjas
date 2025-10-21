d7372d5646a4d6b7248374b1cf03a18bd23c8092
Hawks1299 <mastskel@gmail.com>
2025-10-21 18:02:59 -0400

    feat: Coloca o JpaRepository para abstracao das query's do banco de dados

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Carros/CarrosRepository.java
@@ -0,0 +1,6 @@
+package dev.java10x.CadastroDeNinjas.Carros;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CarrosRepository extends JpaRepository<CarrosModel, Long> {
+}

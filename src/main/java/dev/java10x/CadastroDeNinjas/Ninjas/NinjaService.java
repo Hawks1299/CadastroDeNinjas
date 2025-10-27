7bdcc336d840d8c8b2142841953021b1b1999089
Hawks1299 <mastskel@gmail.com>
2025-10-27 19:16:59 -0400

    feat: Inclui a rota de cadastro de ninjas usando o metodo POST, tambem inicializa o Postman

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -25,4 +25,9 @@ public class NinjaService {
         return ninjaPorId.orElse(null);
     }
 
+    // Criar um novo ninjas
+    public NinjaModel criarNinja(NinjaModel ninja) {
+        return ninjaRepository.save(ninja);
+    }
+
 }
